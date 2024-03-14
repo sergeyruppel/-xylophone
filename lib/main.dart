0ed5455e26ef28666cf0592a4174d2d5fd030c1b
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2024-03-14 21:56:05 +0700

    Implement sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,99 @@
-// import 'package:flutter/material.dart';
-// import 'package:audioplayers/audioplayers.dart';
-
-// void main() {
-//   runApp(const App());
-// }
-
-// class App extends StatefulWidget {
-//   const App({super.key});
-
-//   @override
-//   State<App> createState() => _AppState();
-// }
-
-// class _AppState extends State<App> {
-//   final player = AudioPlayer();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: SafeArea(
-//           child: Center(
-//             child: TextButton(
-//               onPressed: () {
-//                 print('hi');
-//               },
-//               child: const Text('PLAY'),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('AudioPlayer Example'),
+          centerTitle: true,
+          title: const Text('Xylophone'),
         ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              playSound();
-            },
-            child: const Text("Play Sound"),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note1.wav'));
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note2.wav'));
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note3.wav'));
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note4.wav'));
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.cyan,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note5.wav'));
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note6.wav'));
+                },
+                child: Container(),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.play(AssetSource('note7.wav'));
+                },
+                child: Container(),
+              ),
+            ],
           ),
         ),
       ),
     );
-  }
-
-  AudioPlayer audioPlayer = AudioPlayer();
-
-  Future<void> playSound() async {
-    await audioPlayer.play(AssetSource('assets/note1.wav'));
   }
 }
