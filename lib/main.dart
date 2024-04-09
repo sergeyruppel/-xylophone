d0eb3b05eeae35abd92cc2d7dc7d3a3354013eb4
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2024-04-09 20:25:03 +0700

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({super.key});
+  App({super.key});
 
-  // Widget buildKey(Color color, double value) {
-  //   return Expanded(
-  //     child: Padding(
-  //       padding: const EdgeInsets.symmetric(
-  //         horizontal: value,
-  //         vertical: 8.0,
-  //       ),
-  //       child: TextButton(
-  //         style: TextButton.styleFrom(
-  //           backgroundColor: color,
-  //         ),
-  //         onPressed: () {
-  //           playSound(soundNumber: 1);
-  //         },
-  //         child: Container(),
-  //       ),
-  //     ),
-  //   );
-  // }
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.cyan,
+    Colors.blue,
+    Colors.purple,
+  ];
 
   void playSound({required int soundNumber}) {
     final player = AudioPlayer();
     player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Widget buildKey(Color color, double padding, int soundNumber) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: padding,
+          vertical: 8.0,
+        ),
+        child: TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: color,
+          ),
+          onPressed: () {
+            playSound(soundNumber: soundNumber);
+          },
+          child: Container(),
+        ),
+      ),
+    );
   }
 
   @override
@@ -46,127 +56,15 @@ class App extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 24.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // buildKey(Colors.amber, 20),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.red,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 1);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 2);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 32.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.yellow,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 3);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 40.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.green,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 4);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 48.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.cyan,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 5);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 56.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 6);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 64.0,
-                      vertical: 8.0,
-                    ),
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.purple,
-                      ),
-                      onPressed: () {
-                        playSound(soundNumber: 7);
-                      },
-                      child: Container(),
-                    ),
-                  ),
-                ),
+              children: [
+                // for (final color in colors) buildCustomWidget(color, 1)
+                buildKey(colors[0], 16.0, 1),
+                buildKey(colors[1], 24.0, 2),
+                buildKey(colors[2], 32.0, 3),
+                buildKey(colors[3], 40.0, 4),
+                buildKey(colors[4], 48.0, 5),
+                buildKey(colors[5], 56.0, 6),
+                buildKey(colors[6], 64.0, 7),
               ],
             ),
           ),
